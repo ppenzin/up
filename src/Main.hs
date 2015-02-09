cbe7df5cfeaed4d08834c51cf3249db3f8accce5
Petr Penzin <penzin.dev@gmail.com>
2015-02-08 18:49:47 -0800

    Version 0.0.1

--- src/Main.hs
+++ src/Main.hs
@@ -17,24 +17,33 @@ import System.Exit
 
 import Pkg
 
+-- |Entry point function
 main :: IO ()
 main = getArgs >>= parse >> exit
 
+-- |Parse command-line arguments
 parse ["fetch"]  = fetch >> exit
 parse ["status"]  = status >> exit
 parse ["upgrade"] = upgrade >> exit
 parse ["fetch", "upgrade"] = fetch >> upgrade >> exit
+parse ["help"]    = usage >> exit
 parse []          = usage >> exit
 
+-- |Display help
 usage = putStrLn "Usage: up <command>"
      >> putStrLn "The commands are:"
      >> putStrLn "fetch\tUpdate ports tree"
      >> putStrLn "status\tShow ports ready to be upgraded"
      >> putStrLn "upgrade\tRun upgrade on ports that don't have entries in UPDATING"
+-- |Update ports tree
 fetch = portsnapFetchUpdate -- TODO what about first-time update?
+-- |Check for outdated packages
 status = getPackages >>= printPackages
+-- |Upgrade packages
 upgrade = getPackages >>= upgradePackages
+-- |Exhit with 0 status
 exit = exitWith ExitSuccess
+-- |Message to display for ports that can't be upgraded automatically
 upgradeMsg = "\nThese seem to have entries in UPDATING, please check them manually:"
 
 {-|Print two list of packages: `automatic' and `manual'
